cde2a68b55baf091585edfa2a1fafff1db66ba8b
vagrant <vagrant@homestead>
2024-06-13 03:05:34 +0000

    minor change

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Sale extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id', 'payment_method', 'sale_date', 'customer_id', 'total_amount'
+    ];
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+    public function customer(): BelongsTo{
+        return $this->belongsTo(Customer::class);
+    }
 }
