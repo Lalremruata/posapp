6e1c8f38e6e55db838649ad0b17277e2ca55ca53
vagrant <vagrant@homestead>
2024-06-28 12:06:11 +0000

    sales front end added

--- app/Filament/Pages/Sales.php
+++ app/Filament/Pages/Sales.php
@@ -3,10 +3,37 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
 
-class Sales extends Page
+class Sales extends Page implements HasForms
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    use InteractsWithForms;
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
 
     protected static string $view = 'filament.pages.sales';
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->required(),
+                MarkdownEditor::make('content'),
+                // ...
+            ])
+            ->statePath('data');
+    }
+    public function create(): void
+    {
+        dd($this->form->getState());
+    }
 }
